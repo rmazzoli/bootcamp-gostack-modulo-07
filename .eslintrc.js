852d6345a951974bcfb257c42963788c9ee4e2fa
rmazzoli <rafael.mazzoli@hotmail.com>
2019-11-03 17:44:41 -0300

    Buscando produtos da API

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: [
-    'airbnb',
-    'prettier',
-    'prettier/react'
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -22,14 +18,21 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
-      'prettier/prettier': 'error',
-      'react/jsx-filename-extension': [
-          'warn',
-          { extensions: ['.jsx', '.js'] }
-      ],
-      'import/prefer-default-export': 'off'
-  },
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ["error", { allow: ["tron"] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 };
