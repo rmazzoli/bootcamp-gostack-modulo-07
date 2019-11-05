0bee879ebea310dc1967dffd406d212cd0a78d47
rmazzoli <rafael.mazzoli@hotmail.com>
2019-11-04 21:10:03 -0300

    Remover produto

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -9,7 +9,7 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   console.tron.log(cart[0]);
   return (
     <Container>
@@ -48,7 +48,15 @@ function Cart({ cart }) {
                 <strong>R$ 265,33</strong>
               </td>
               <td>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({
+                      type: 'REMOVE_FROM_CART',
+                      id: product.id,
+                    })
+                  }
+                >
                   <MdDelete size={20} color="7159c1" />
                 </button>
               </td>
