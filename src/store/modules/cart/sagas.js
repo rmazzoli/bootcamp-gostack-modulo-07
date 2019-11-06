1740c936d85b71d8a67d90438c262f55e292a9f4
rmazzoli <rafael.mazzoli@hotmail.com>
2019-11-05 22:50:44 -0300

    Navegando no Saga

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,6 +1,7 @@
 import { call, put, all, takeLatest, select } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { addToCartSuccess, updateAmountSuccess } from './actions';
 import { formatPrice } from '../../../util/format';
 
@@ -33,6 +34,7 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+    history.push('/cart');
   }
 }
 
