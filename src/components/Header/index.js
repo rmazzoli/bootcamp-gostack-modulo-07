2a26d751cc324d0877124519da75cdad370817d5
rmazzoli <rafael.mazzoli@hotmail.com>
2019-11-07 23:08:25 -0300

    Hooks com Redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
+
 import { MdShoppingBasket } from 'react-icons/md';
 
 import { Container, Cart } from './styles';
+
 import logo from '../../assets/images/logo.svg';
 
-function Header(cartSize) {
+export default function Header() {
+  const cartSize = useSelector(state => state.cart.length);
+
   return (
     <Container>
       <Link to="/">
         <img src={logo} alt="Rocketshoes" />
       </Link>
+
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
-          <span>{cartSize.cartSize} itens</span>
+          <span>{cartSize} itens</span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
       </Cart>
     </Container>
   );
 }
-
-export default connect(state => ({
-  cartSize: state.cart.length,
-}))(Header);
